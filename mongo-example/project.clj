91c808bd8631e139a2d27f1afe582c99eed46c62
Christian Stamm <christian.stamm@otto.de>
2015-06-02 21:39:48 +0200

    update to version 0.1.13

--- mongo-example/project.clj
+++ mongo-example/project.clj
@@ -6,13 +6,16 @@
             :scm {:name "git"
                   :url  "https://github.com/otto-de/tesla-mongo-connect"}
             :dependencies [[org.clojure/clojure "1.6.0"]
-                           [de.otto/tesla-microservice "0.1.12"]
-                           [de.otto/tesla-mongo-connect "0.1.4"]]
+                           [de.otto/tesla-microservice "0.1.13"]
+                           [de.otto/tesla-mongo-connect "0.1.4"]
+                           [org.slf4j/slf4j-api "1.7.12"]
+                           [ch.qos.logback/logback-core "1.1.3"]
+                           [ch.qos.logback/logback-classic "1.1.3"]]
 
             :plugins [[lein-embongo "0.2.2"][lein-environ "1.0.0"]]
 
             :embongo {:port     27017
                       :version  "2.6.4"
                       :data-dir "./target/mongo-data-files"}
-            
+
             :main ^:skip-aot de.otto.tesla.mongo.example.example-system)
--- simple-example/project.clj
+++ simple-example/project.clj
@@ -6,5 +6,8 @@
             :scm { :name "git"
  	           :url "https://github.com/otto-de/tesla-microservice"}
             :dependencies [[org.clojure/clojure "1.6.0"]
-                           [de.otto/tesla-microservice "0.1.12"]]
+                           [de.otto/tesla-microservice "0.1.13"]
+                           [org.slf4j/slf4j-api "1.7.12"]
+                           [ch.qos.logback/logback-core "1.1.3"]
+                           [ch.qos.logback/logback-classic "1.1.3"]]
             :main ^:skip-aot de.otto.tesla.example.example-system)
