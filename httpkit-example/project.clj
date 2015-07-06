9e18804dd68b1b10a123d6983ad9d77994d35d34
Christian Stamm <christian.stamm@otto.de>
2015-07-06 20:24:08 +0200

    use newest versions of tesla-microservice and tesla-httpkit

--- httpkit-example/project.clj
+++ httpkit-example/project.clj
@@ -1,4 +1,4 @@
-(defproject de.otto/tesla-httpkit-example "0.1.15"
+(defproject de.otto/tesla-httpkit-example "0.1.18"
             :description "a simple example of an application build with tesla-microservice."
             :url "https://github.com/otto-de/tesla-examples"
             :license {:name "Apache License 2.0"
@@ -6,8 +6,9 @@
             :scm {:name "git"
                   :url  "https://github.com/otto-de/tesla-examples"}
             :dependencies [[org.clojure/clojure "1.6.0"]
-                           [de.otto/tesla-microservice "0.1.15"]
-                           [de.otto/tesla-httpkit "0.1.0"]
+                           [de.otto/tesla-microservice "0.1.18"]
+                           [de.otto/tesla-httpkit "0.1.1"]
+                           [hiccup "1.0.5"]
                            [org.slf4j/slf4j-api "1.7.12"]
                            [ch.qos.logback/logback-core "1.1.3"]
                            [ch.qos.logback/logback-classic "1.1.3"]]
