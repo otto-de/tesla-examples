4ee86140c736325a26c0d5b7155db1d49caad48c
Niko Schmuck <niko@nava.de>
2015-07-09 22:38:34 +0200

    Added hiccup as dependency
    To fix compilation issue with page.clj

--- simple-example/project.clj
+++ simple-example/project.clj
@@ -8,6 +8,7 @@
             :dependencies [[org.clojure/clojure "1.6.0"]
                            [de.otto/tesla-microservice "0.1.18"]
                            [de.otto/tesla-jetty "0.1.0"]
+                           [hiccup "1.0.5"]
                            [org.slf4j/slf4j-api "1.7.12"]
                            [ch.qos.logback/logback-core "1.1.3"]
                            [ch.qos.logback/logback-classic "1.1.3"]]
