390b1d1128a1794a42f129c88dc16cd4f19b89cf
Daniel Schruhl <danielschruhl@gmail.com>
2016-02-25 13:40:57 +0100

    simple-example has a form page with POST requests instead of using GET requests
    * dependencies updated
    * page is tested
    * component automatically restarts

--- simple-example/src/de/otto/tesla/example/example_system.clj
+++ simple-example/src/de/otto/tesla/example/example_system.clj
@@ -1,7 +1,7 @@
 (ns de.otto.tesla.example.example-system
   (:require [de.otto.tesla.system :as system]
             [de.otto.tesla.example.calculating :as calculating]
-            [de.otto.tesla.serving-with-jetty :as serving]
+            [de.otto.tesla.serving-with-jetty :as jetty]
             [de.otto.tesla.example.page :as page]
             [com.stuartsierra.component :as component])
   (:gen-class))
@@ -11,13 +11,13 @@
 
 (defn example-system [runtime-config]
   (-> (system/base-system (merge {:name "example-service"} runtime-config))
-      (assoc :calculator
-             (component/using (calculating/new-calculator example-calculation-function) [:metering :app-status]))
-      (assoc :example-page
-             (component/using (page/new-page) [:handler :calculator :app-status]))))
+      (assoc
+        :calculator   (component/using (calculating/new-calculator example-calculation-function) [:metering :app-status])
+        :example-page (component/using (page/new-page) [:handler :calculator :app-status]))
+      (jetty/add-server)))
 
 (defn -main
   "starts up the production system."
   [& args]
-  (system/start (serving/add-server (example-system {}) :example-page)))
+  (system/start (example-system {})))
 
