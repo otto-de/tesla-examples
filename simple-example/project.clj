2039b858ac28ebf7fbe5f53c40bdfec9953bca7f
Torsten Mangner <tmangner@inoxio.de>
2019-01-25 10:06:13 +0100

    Upgrade dependencies for simple-example

--- simple-example/project.clj
+++ simple-example/project.clj
@@ -5,18 +5,18 @@
                       :url  "http://www.apache.org/license/LICENSE-2.0.html"}
             :scm {:name "git"
                   :url  "https://github.com/otto-de/tesla-microservice"}
-            :dependencies [[org.clojure/clojure "1.8.0"]
-                           [de.otto/tesla-microservice "0.7.1"]
-                           [de.otto/tesla-jetty "0.1.3"]
+            :dependencies [[org.clojure/clojure "1.10.0"]
+                           [de.otto/tesla-microservice "0.13.0"]
+                           [de.otto/tesla-jetty "0.2.6"]
                            [hiccup "1.0.5"]
-                           [org.slf4j/slf4j-api "1.7.22"]
-                           [ch.qos.logback/logback-core "1.1.8"]
-                           [ch.qos.logback/logback-classic "1.1.8"]]
+                           [org.slf4j/slf4j-api "1.7.25"]
+                           [ch.qos.logback/logback-core "1.2.3"]
+                           [ch.qos.logback/logback-classic "1.2.3"]]
             :profiles {:dev {:main         de.otto.tesla.example.testcore
                              :source-paths ["src" "test"]
                              :dependencies [[ring-mock "0.1.5"]
-                                            [me.lomin/component-restart "0.1.1"]
-                                            [hickory "0.7.0"]]
-                             :plugins      [[lein-ancient "0.6.10"]]}}
+                                            [me.lomin/component-restart "0.1.2"]
+                                            [hickory "0.7.1"]]
+                             :plugins      []}}
 
             :main ^:skip-aot de.otto.tesla.example.core)
